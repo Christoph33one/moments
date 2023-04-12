2b45cdf60f90d733a612304f7f190ecffa763781
Christoph33one <cjbrowning23@hotmail.com>
2023-04-12 09:50:56 +0000

    added interceptors to controll token refreshing

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,6 +4,11 @@ axios.defaults.baseURL = "https://django-rest-project.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
+/**intercept the request */
+export const axiosReq = axios.create();
+/**intercept the response  */
+export const axiosRes = axios.create();
+
 /** For setting up and connection you API. Look at Authentication: video with moments*/
 /**remeber to always double check your CONVIG VARS */
 /**Always check the heruko API ulr is correct */
